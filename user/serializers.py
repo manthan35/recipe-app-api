7adea6e235b1c980214e81ca37ae70f876df48f2
manthan <manthan.moradiya2@gmail.com>
2019-12-26 11:31:24 +0530

    added working list tag endpoint

--- user/serializers.py
+++ user/serializers.py
@@ -17,6 +17,16 @@ class UserSerializer(serializers.ModelSerializer):
         """createa new user encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user , setting the password correctly and return it"""
+        password = validated_data.pop('password',None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the  user authentication object"""
     email = serializers.CharField()
